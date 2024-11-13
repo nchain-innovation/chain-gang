a3685009f6469110fe7e2c08712afcb23321783d
Arthur Gordon <a.gordon@nchain.com>
2024-11-13 11:49:35 +0000

    extern crate no longer required

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,7 @@
 //! A foundation for building applications on Bitcoin SV using Rust.
 
-//#![cfg(feature = "interface")]
-
-extern crate byteorder;
-extern crate dns_lookup;
-extern crate hex;
 #[macro_use]
 extern crate log;
-extern crate hmac;
-extern crate linked_hash_map;
-extern crate murmur3;
-extern crate rand;
-
-extern crate base58;
-extern crate k256;
-extern crate ripemd;
-extern crate snowflake;
 
 #[cfg(feature = "python")]
 extern crate lazy_static;
